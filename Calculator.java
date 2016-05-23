3d5f0a738e66e1ebaf005716f0698b65ad11c99c
SlavaDikhnich <vyacheslav.dihnich@yandex.ru>
2016-05-24 02:08:58 +0300

    new files

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+// класс Calculator
+public class Calculator {
+	private int result;
+	
+	public void add (int ... params) {
+		for (Integer param : params) {
+			result += param;
+		}
+	}
+	
+	public int getResult() {
+		return result;
+	}
+	
+	public void cleanResult() {
+		result = 0;
+	}
+}
