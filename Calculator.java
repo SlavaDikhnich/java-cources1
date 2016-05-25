e8f66075b88358c782038c80053262020ecb0c0d
SlavaDikhnich <vyacheslav.dihnich@yandex.ru>
2016-05-26 01:49:47 +0300

    new operation

--- Calculator.java
+++ Calculator.java
@@ -1,18 +1,41 @@
 // класс Calculator
 public class Calculator {
 	private int result;
+	private int arg;
 	
 	public void add (int ... params) {
 		for (Integer param : params) {
-			result += param;
+			this.result += param;
 		}
 	}
 	
 	public int getResult() {
-		return result;
+		return this.result;
 	}
 	
 	public void cleanResult() {
-		result = 0;
+		this.result = 0;
+		this.arg = 0;
+	}
+	
+	public void subtract (int ... params) {
+		for (int i = 0; i < params.length; i++){
+			if (i == 0)  this.arg = params[i];
+			else this.result = arg - params[i];		 
+		}
+	}
+	
+	public void divide (int ... params) {
+		for (int i = 0; i < params.length; i++){
+			if (i == 0)  this.arg = params[i];
+			else this.result = arg / params[i];		 
+		}		
+	}
+	
+	public void multiply (int ... params) {
+		for (int i = 0; i < params.length; i++){
+			if (i == 0)  this.arg = params[i];
+			else this.result = arg * params[i];		 
+		}		
 	}
 }
--- InteractRunner.java
+++ InteractRunner.java
@@ -10,11 +10,32 @@ public class InteractRunner {
 			Calculator calc = new Calculator();
 			String exit = "no";
 			while (!exit.equals("yes")) {
+				System.out.println("Enter type arithmetic operation :");
+				String op_type = reader.next();				
 				System.out.println("Enter first arg :");
 				String first = reader.next();
 				System.out.println("Enter second arg :");
 				String second = reader.next();
-				calc.add(Integer.valueOf(first), Integer.valueOf(second));
+				
+				switch (op_type) {
+					case "+": {
+						calc.add(Integer.valueOf(first), Integer.valueOf(second));
+						break;
+					}
+					case "-": {
+						calc.subtract(Integer.valueOf(first), Integer.valueOf(second));
+						break;
+					}
+					case "*": {
+						calc.multiply(Integer.valueOf(first), Integer.valueOf(second));
+						break;
+					}
+					case "/": {
+						calc.divide(Integer.valueOf(first), Integer.valueOf(second));
+						break;
+					}
+				}
+				
 				System.out.println("Result: " + calc.getResult());
 				calc.cleanResult();
 				System.out.println("Exit: yes/no ");
